dc6c18ce7480675e64f5e5744ab4b9ccff86036b
Shalom-platinum <72386382+Shalom-platinum@users.noreply.github.com>
2023-04-17 02:42:46 +0100

    Delete Todo Project/src directory

--- Todo Project/src/pages/Nopage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Nopage() {
-    return (
-        <h1>Error 404 No page</h1>
-    );
-}
-
-export default Nopage;
--- Todo Project/src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default function Sogin() {
-    return (
-        <h1>register page</h1>
-    )
-}
